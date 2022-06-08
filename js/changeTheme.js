c0bdcba66ac1e017ef77148375994fb429e32f38
VyTsan <tosanvy@gmail.com>
2022-06-08 16:20:48 +0700

    Cả nút đăng nhập đăng ký đăng xuất cũng đổi theme

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -2,8 +2,7 @@ const changeThemeBtnEle = document.querySelector('#changeThemeBtn');
 const titleEle = document.querySelector('.title');
 const bodyEle = document.getElementsByTagName('body')[0];
 const littleIconsEle = document.querySelectorAll('.icon');
-const changeCardBtnEle = document.querySelector('.change-card');
-const tempBtnEle = document.querySelectorAll('.tempBtn');
+const fontBtnEle = document.querySelectorAll('.fontBtn');
 var theme = 1;
 
 // Change Theme
@@ -14,17 +13,21 @@ function changeTheme() {
     bodyEle.style = "background-image: url('https://static.vecteezy.com/system/resources/previews/000/273/915/non_2x/ocean-background-vector.jpg')"; 
     littleIconsEle.forEach(icon => {
       icon.style.backgroundColor = 'rgb(177, 208, 241)';
+    });  
+    fontBtnEle.forEach(btn => {
+      btn.classList.remove('btn-success');
+      btn.classList.add('btn-primary');
     });
-    changeCardBtnEle.classList.remove('btn-success');
-    changeCardBtnEle.classList.add('btn-primary');
   } else {
     titleEle.innerHTML = `<img src="https://see.fontimg.com/api/renderfont4/d9VDV/eyJyIjoiZnMiLCJoIjo2NCwidyI6MTI1MCwiZnMiOjUxLCJmZ2MiOiIjNTlDRDFBIiwiYmdjIjoiI0VBRTBFOCIsInQiOjF9/QW5pbWFscw/palmsprings-personal-use-regular.png" alt="title">`;
     bodyEle.style.backgroundImage = `url("https://wallpaperaccess.com/full/3665287.jpg")`;
     littleIconsEle.forEach(icon => {
       icon.style.backgroundColor = 'rgb(207, 233, 183)';
     });
-    changeCardBtnEle.classList.remove('btn-primary');
-    changeCardBtnEle.classList.add('btn-success');
+    fontBtnEle.forEach(btn => {
+      btn.classList.remove('btn-primary');
+      btn.classList.add('btn-success');
+    });
   }
 }
 changeThemeBtnEle.addEventListener('click', changeTheme);
